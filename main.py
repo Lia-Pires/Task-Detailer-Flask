e4a167dab0016d9743340a49623b0065c0b7a2c3
Lia Pires <liasth.pires@gmail.com>
2022-08-09 15:41:43 -0300

    Create main application
    Create main application

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_topics():
+    return render_template('home.html')
+     
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5533)
